e5d8b287f899a29c51a80665b6d24aaa2bf7d417
David Villarreal <steadypathapp@gmail.com>
2014-08-28 15:49:03 -0500

    Changes

--- core/src/com/Phaze1D/RisingFallAndroidV2/Actors/Panels/PowerTimePanel.java
+++ core/src/com/Phaze1D/RisingFallAndroidV2/Actors/Panels/PowerTimePanel.java
@@ -1,13 +1,125 @@
 package com.Phaze1D.RisingFallAndroidV2.Actors.Panels;
 
+import com.Phaze1D.RisingFallAndroidV2.Singletons.BitmapFontSizer;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 /**
  * Created by davidvillarreal on 8/26/14.
  * Rising Fall Android Version
  */
 public class PowerTimePanel extends Panel {
+
+    public float time;
+    public float currentTime;
+    public float targetTime;
+
+    public int constantTime;
+    public int ballsLeft;
+    public int powerType;
+
+    public Image titlePower;
+
+    public Label timeLeftLabel;
+    public Label ballsLeftLabel;
+
+    public TextureAtlas powerBallAtlas;
+
+
     public PowerTimePanel(Sprite panelSprite) {
         super(panelSprite);
     }
+
+    public void createPanelWithTimer(){
+        Sprite powerBallSprite = powerBallAtlas.createSprite("powerBall" + powerType);
+
+        titlePower = new Image(powerBallSprite);
+        titlePower.setPosition((int)(getWidth()/2 - titlePower.getWidth()/2), (int)(getHeight()/2));
+        titlePower.setScale(titlePower.getWidth()/1.5f, titlePower.getHeight()/1.5f);
+        addActor(titlePower);
+
+
+        constantTime = 11;
+        currentTime = System.currentTimeMillis()/1000;
+        String timeString = String.format("%02d:%02d",0, constantTime);
+        timeLeftLabel = new Label(timeString, new Label.LabelStyle(BitmapFontSizer.getFontWithSize(0), Color.BLACK));
+        timeLeftLabel.setPosition((int)(getWidth()/2 - timeLeftLabel.getWidth()/2), (int)(getHeight()/7 - timeLeftLabel.getHeight()/2));
+        targetTime = constantTime + currentTime;
+        addActor(timeLeftLabel);
+    }
+
+
+    public void createPanelWithBalls(){
+        Sprite powerBallSprite = powerBallAtlas.createSprite("powerBall" + powerType);
+        titlePower = new Image(powerBallSprite);
+        titlePower.setPosition((int)(getWidth()/2 - titlePower.getWidth()/2), (int)(getHeight()/2));
+        titlePower.setScale(titlePower.getWidth()/1.5f, titlePower.getHeight()/1.5f);
+        addActor(titlePower);
+
+        ballsLeft = 10;
+        ballsLeftLabel = new Label(ballsLeft + "", new Label.LabelStyle(BitmapFontSizer.getFontWithSize(0), Color.BLACK));
+        ballsLeftLabel.setPosition((int)(getWidth()/2 - ballsLeftLabel.getWidth()/2), (int)(getHeight()/7 - ballsLeftLabel.getHeight()/2));
+        addActor(ballsLeftLabel);
+
+    }
+
+    public boolean updateBallsLeft(){
+        ballsLeft--;
+
+        if (ballsLeft <= 0){
+            ballsLeft = 0;
+            ballsLeftLabel.setText(ballsLeft+"");
+            return true;
+        }else {
+            ballsLeftLabel.setText(ballsLeft+"");
+            return false;
+        }
+
+    }
+
+    public boolean updatetimer(){
+        time = targetTime - currentTime;
+        if (time <= 0){
+            time = 0;
+            String timeString = String.format("%02d:%02f",0, time);
+            timeLeftLabel.setText(timeString);
+            return true;
+        }else {
+            String timeString = String.format("%02d:%02f",0, time);
+            timeLeftLabel.setText(timeString);
+            return false;
+        }
+    }
+
+    public void resetTimer(){
+        if (timeLeftLabel != null){
+            targetTime = constantTime + currentTime;
+            clear();
+            createPanelWithTimer();
+        }else {
+            clear();
+            titlePower = null;
+            createPanelWithTimer();
+        }
+        ballsLeftLabel = null;
+    }
+
+    public void resetBalls(){
+        if (ballsLeftLabel != null){
+            ballsLeft = 11;
+            clear();
+            createPanelWithBalls();
+        }else {
+            clear();
+            titlePower = null;
+            createPanelWithBalls();
+        }
+
+        timeLeftLabel = null;
+    }
+
+
 }
--- core/src/com/Phaze1D/RisingFallAndroidV2/Actors/Panels/ScorePanel.java
+++ core/src/com/Phaze1D/RisingFallAndroidV2/Actors/Panels/ScorePanel.java
@@ -1,13 +1,92 @@
 package com.Phaze1D.RisingFallAndroidV2.Actors.Panels;
 
+import com.Phaze1D.RisingFallAndroidV2.Actors.CustomLabel;
+import com.Phaze1D.RisingFallAndroidV2.Singletons.BitmapFontSizer;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
 /**
  * Created by davidvillarreal on 8/26/14.
  * Rising Fall Android Version
  */
 public class ScorePanel extends Panel {
+
+    public int currentScore;
+    public int targetScore;
+
+    public boolean reachYet;
+
+    public CustomLabel scoreLabel;
+    public CustomLabel titleLabel;
+
+
     public ScorePanel(Sprite panelSprite) {
         super(panelSprite);
     }
+
+    public void createScorePanel(int targetScore){
+        currentScore = 0;
+        this.targetScore = targetScore;
+
+        titleLabel = new CustomLabel("scorek", new Label.LabelStyle(BitmapFontSizer.getFontWithSize(0), Color.BLACK));
+        titleLabel.setPosition((int)(getWidth()/2 - titleLabel.getWidth()/2), (int)(getHeight()*4/7 - titleLabel.getHeight()/2));
+        addActor(titleLabel);
+
+        scoreLabel = new CustomLabel(currentScore + "/" + targetScore, new Label.LabelStyle(BitmapFontSizer.getFontWithSize(0), Color.BLACK));
+        scoreLabel.setPosition((int)(getWidth()/2 - scoreLabel.getWidth()/2),(int)(getHeight()/7 - scoreLabel.getHeight()/2));
+        scoreLabel.setAlignment(Align.center);
+        addActor(scoreLabel);
+
+    }
+
+    public void updateScore(int addScore){
+        currentScore += addScore;
+        scoreLabel.setText(currentScore + "/" + targetScore);
+
+        if (currentScore >= targetScore && !reachYet){
+            titleLabel.setVisible(false);
+            scoreLabel.setVisible(false);
+
+            final CustomLabel reachL = new CustomLabel(targetScore + "", new Label.LabelStyle(BitmapFontSizer.getFontWithSize(0), Color.BLACK));
+            AlphaAction alphaAction = Actions.alpha(0, 1.5f);
+            ScaleToAction scaleToAction = Actions.scaleTo(1.5f,1.5f,1.5f);
+            ParallelAction group = Actions.parallel(alphaAction, scaleToAction);
+            Action complete = new Action() {
+                @Override
+                public boolean act(float delta) {
+                    titleLabel.setVisible(true);
+                    scoreLabel.setVisible(true);
+                    reachL.remove();
+                    reachL.clear();
+                    return true;
+                }
+            };
+            reachL.addAction(Actions.sequence(group, complete));
+            addActor(reachL);
+        }
+
+    }
+
+    public boolean didReachScore(){
+        if (currentScore >= targetScore){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void didNotReachAnimation(){
+
+        ScaleToAction up = Actions.scaleTo(1.5f, 1.5f, 1f);
+        ScaleToAction down = Actions.scaleTo(1,1,1f);
+        SequenceAction seq = Actions.sequence(up, down);
+        scoreLabel.addAction(Actions.forever(seq));
+
+    }
+
+
 }
